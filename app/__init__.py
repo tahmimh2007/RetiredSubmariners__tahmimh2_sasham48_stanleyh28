527f8cce6c4a7ab278faff9daa86bd019ddfb5a2
Stanley Hoo <shoo50@stuy.edu>
2025-04-01 10:57:56 -0400

    routes

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/visual")
+def visual():
+    return render_template("visual.html")
+
+@app.route("/upload")
+def upload():
+    return render_template("upload.html")
+
+@app.route("/ml")
+def ml():
+    return render_template("ml.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
